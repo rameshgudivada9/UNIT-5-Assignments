adac62253988755f22b05aba5741283eee0a6c15
rameshgudivada9 <96101948+rameshgudivada9@users.noreply.github.com>
2022-04-11 00:25:04 +0530

    change

--- day3_sates,props,rendaring/inventory/src/components/Inventory.jsx
+++ day3_sates,props,rendaring/inventory/src/components/Inventory.jsx
@@ -1,12 +1,16 @@
 import { useState } from "react";
 
 export const Inventory = () => {
-  const [inv, setInv] = useState({
+  const [inv, setInv] = useState(
+    {
     books: 10,
     notebooks: 13,
     pens: 40,
-  });
+  }
+  );
     // Create add and remove functions here that changes the
+
+    console.log(inv.books)
     // state.
   return (
     <div
@@ -21,11 +25,17 @@ export const Inventory = () => {
     >
       <div className="items">
         <span>Books: </span>
-        <button className="circlularButton">+</button>
+        <button className="circlularButton" onClick={(inv)=>{
+          // inv.books++
+          inv.books=inv.books+1
+          console.log("clicked")
+          setInv(inv.books)
+          console.log(inv.books)
+        }}>+</button>
         <button className="circlularButton">-</button>
-        <span>{inv.books}</span>
+        <p>{inv.books}</p>
       </div>
-      <div className="items">
+      {/* <div className="items">
         <span>Notebooks: </span>
         <button className="circlularButton">+</button>
         <button className="circlularButton">-</button>
@@ -42,7 +52,7 @@ export const Inventory = () => {
         <button className="circlularButton">+</button>
         <button className="circlularButton">-</button>
         <span>{inv.inkpens}</span>
-      </div>
+      </div> */}
             {/*calculate total and show it*/}
       total: {0}
     </div>
