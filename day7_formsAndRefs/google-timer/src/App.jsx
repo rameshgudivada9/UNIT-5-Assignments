bf2309f4b003303b5bb246279697ea56b355b2cf
rameshgudivada9 <96101948+rameshgudivada9@users.noreply.github.com>
2022-04-18 23:18:00 +0530

    changes

--- day7_formsAndRefs/google-timer/src/App.jsx
+++ day7_formsAndRefs/google-timer/src/App.jsx
@@ -1,11 +1,45 @@
 import './App.css';
-
+import {useState,useRef, useEffect} from "react";
 function App() {
+  const[counter,setCounter]=useState(10);
+  const counterRef=useRef(null);
+
+  useEffect(()=>{
+    counterRef.current=  setInterval(()=>{
+      setCounter((r)=>r+1)
+    },1000)
+  },[])
+
+  
   return (
     <div className="App">
-    
+  <h3>counter:{counter}</h3>
+
+  <button onClick={()=>{
+  clearInterval(counterRef.current)
+  }} >pause</button>
+
+
+<button onClick={()=>{
+  counterRef.current=  setInterval(()=>{
+    setCounter((r)=>r+1)
+  },1000)
+
+
+}} >Start</button>
+
+<button onClick={()=>{
+
+clearInterval(counterRef.current);
+setCounter(0);
+
+}} >Reset</button>
     </div>
   );
 }
 
 export default App;
+
+// what useRef do?
+// 1.store the value , and do not lose it,when component is re-rendaring
+// 2.changein this value,should not cause re-rendar
