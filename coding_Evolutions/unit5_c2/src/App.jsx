af13fe9829326ade1733685e3a7f6b57f833176d
rameshgudivada9 <96101948+rameshgudivada9@users.noreply.github.com>
2022-04-18 16:34:59 +0530

    change

--- coding_Evolutions/unit5_c2/src/App.jsx
+++ coding_Evolutions/unit5_c2/src/App.jsx
@@ -12,7 +12,7 @@ function App() {
       >
         {toggleButtonForForm ? "AddStudent" : "ShowStudents"}
       </button>
-      <AddStudent/>
+    
       {/* Show either  AddStudent component or ShowStudents dependeing on the above button click  */}
       {/* make sure the table is shown initially, do not show form initially */}
       {/* make sure to show either of them do not both together */}
